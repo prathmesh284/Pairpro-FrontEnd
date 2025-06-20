3c3e05cab5e9bb6831ae9b9c4e8e334fd4ba35b7
Pratham <jadhavprathmesh284@gmail.com>
2025-06-20 09:25:22 +0530

    server

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://pairpro-backend-backup.onrender.com", {
+const socket = io("https://pairpro-backend.onrender.com", {
   transports: ["websocket"],
   autoConnect: true, // Auto connect once globally
   reconnectionAttempts: 5,
